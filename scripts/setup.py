13e466f56cbf755c1dff2c98da23e2cf578ab3cc
carmenyanes00 <104549311+carmenyanes00@users.noreply.github.com>
2023-11-22 13:51:41 -0600

    revert to latest setup.py version

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,8 +1,14 @@
 import subprocess
 
+
+# Check if the user account `testuser` exists
+def check_if_user_exists(username):
+    subprocess.run(["sudo", "useradd", "-m", "-s", "/bin/bash", username, "-d", "/home/%s" % username])
+
 # Create a new user account
 def create_user(username, home_directory):
-    subprocess.run(["sudo", "useradd", "-m", "-s", "/bin/bash", username, "-d", home_directory])
+    if not check_if_user_exists(username):
+        subprocess.run(["sudo", "useradd", "-m", "-s", "/bin/bash", username, "-d", home_directory])
 
 # Add the user to the sudo group
 def add_user_to_sudo(username):
@@ -15,30 +21,26 @@ def set_password(username):
 # Grant permission to restart services
 def grant_permission_to_restart_services(username):
     input_string = "%s ALL=NOPASSWD:/bin/systemctl restart *" % username
-    input_bytes = input_string.encode('utf-8')
-    subprocess.run(["sudo", "visudo", "-f", "/etc/sudoers"], input=input_bytes)
+    subprocess.run(["sudo", "visudo", "-f", "/etc/sudoers", "-c", input_string])
 
 # Grant permission to view, list, and stop network services
 def grant_permission_to_network_services(username):
     input_string = "%s ALL=NOPASSWD:/bin/systemctl status network-*, /bin/systemctl list-units network-*, /bin/systemctl stop network-*" % username
-    input_bytes = input_string.encode('utf-8')
-    subprocess.run(["sudo", "visudo", "-f", "/etc/sudoers"], input=input_bytes)
+    subprocess.run(["sudo", "visudo", "-f", "/etc/sudoers", "-c", input_string])
 
 # Grant permission to view, list, and stop system services
 def grant_permission_to_system_services(username):
     input_string = "%s ALL=NOPASSWD:/bin/systemctl status system-*, /bin/systemctl list-units system-*, /bin/systemctl stop system-*" % username
-    input_bytes = input_string.encode('utf-8')
-    subprocess.run(["sudo", "visudo", "-f", "/etc/sudoers"], input=input_bytes)
+    subprocess.run(["sudo", "visudo", "-f", "/etc/sudoers", "-c", input_string])
 
 # Grant permission to view, list, and stop logging services
 def grant_permission_to_logging_services(username):
     input_string = "%s ALL=NOPASSWD:/bin/systemctl status journal-*, /bin/systemctl list-units journal-*, /bin/systemctl stop journal-*" % username
-    input_bytes = input_string.encode('utf-8')
-    subprocess.run(["sudo", "visudo", "-f", "/etc/sudoers"], input=input_bytes)
+    subprocess.run(["sudo", "visudo", "-f", "/etc/sudoers", "-c", input_string])
 
 # Main function
 def main():
-    username = "testuser"
+    username = "testuser2"
     home_directory = "/home/%s" % username
 
     create_user(username, home_directory)
